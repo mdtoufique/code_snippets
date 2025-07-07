e035bfe6424c52de0a03b9004bb674e85c73c2fc
toufique <mdrehmant@gmail.com>
2025-07-07 07:02:09 +0600

    divisors added

---
+++ divisors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll              long long
+#define pb              push_back
+#define cn              continue;
+#define br              break;
+#define nn              cout << "\n";
+#define N               cout << "NO\n";
+#define Y               cout << "YES\n";
+#define v(a)            a.begin(),a.end()
+#define rem(s,a)        s.erase(remove(s.begin(), s.end(), a),s.end());
+#define ft(fao,it)      for(auto it=fao.begin();it!=fao.end();it++)      
+#define fr(i,j,n)       for(i=j;i<n;i++)
+#define frm(i,n,j)      for(i=n;i>=j;i--)   
+ll mod=1e9+7;
+ll MOD=1e9+7;
+
+
+std::vector<int> getDivisors(int n) {
+    vector<int> divisors;
+    
+    for (int i = 1; i <= std::sqrt(n); ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n / i) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    std::sort(divisors.begin(), divisors.end());
+    return divisors;
+}
